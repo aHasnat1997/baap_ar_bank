7d3e49695093da445cc945121cd140217d6b9877
aHasnat1997 <hasnat56703@gmail.com>
2023-02-20 19:50:21 +0600

    some error fix

--- js/bank.js
+++ js/bank.js
@@ -2,6 +2,11 @@ document.getElementById('withdraw-btn').addEventListener('click', () => {
   console.log('withdeaw btn clicked...');
   let withdeaw = parseFloat(document.getElementById('withdraw-fild').innerText);
   let withdeawAmount = parseFloat(document.getElementById('withdraw-amount').value);
+  if(document.getElementById('withdraw-amount').value <= 0){
+    alert('Input valid number!!!');
+    document.getElementById('withdraw-amount').value = '';
+    return;
+  }
   withdeaw += withdeawAmount;
   document.getElementById('withdraw-fild').innerText = withdeaw;
 
@@ -14,5 +19,21 @@ document.getElementById('withdraw-btn').addEventListener('click', () => {
 
 document.getElementById('deposit-btn').addEventListener('click', () => {
   console.log('deposit btn clicked...');
+
+  let deposit = parseFloat(document.getElementById('deposit-fild').innerText);
+  let depositAmount = parseFloat(document.getElementById('deposit-amount').value);
+  if(document.getElementById('deposit-amount').value <= 0){
+    alert('Input valid number!!!');
+    document.getElementById('deposit-amount').value = '';
+    return;
+  }
+  deposit += depositAmount;
+  document.getElementById('deposit-fild').innerText = deposit;
+
+  let totalAmount = parseFloat(document.getElementById('total-fild').innerText);
+  totalAmount += depositAmount;
+  document.getElementById('total-fild').innerText = totalAmount;
+
+  document.getElementById('deposit-amount').value = '';
 
 });
--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
 // "use strict";
 
-document.getElementById('submit-btn').addEventListener('click', function(){
+document.getElementById('submit-btn').addEventListener('click', function () {
   let email = document.getElementById('email-fild').value;
   let password = document.getElementById('password-fild').value;
 
   const validEmail = 'hello@world.com';
   const validPassword = 'baapARbank';
-  if(email === validEmail && password === validPassword){
+  if (email === validEmail && password === validPassword) {
     window.location.href = 'bank.html';
   }
-  else{
+  else {
     window.alert('!Invalid User...');
   }
 
+});
+
+
+
+
+document.getElementById('email-fild').addEventListener('click', () => {
+  const lableEmail = document.getElementById('email-fild-label');
+  const lablePassword = document.getElementById('password-fild-label');
+  lableEmail.style.top = '-1.8rem';
+  lableEmail.style.fontSize = '1rem';
+  // lablePassword.style.top = '0rem';
+  // lablePassword.style.fontSize = '1.5rem';
+});
+document.getElementById('password-fild').addEventListener('click', () => {
+  const lableEmail = document.getElementById('email-fild-label');
+  const lablePassword = document.getElementById('password-fild-label');
+  lablePassword.style.top = '-1.8rem';
+  lablePassword.style.fontSize = '1rem';
+  // lableEmail.style.top = '0rem';
+  // lableEmail.style.fontSize = '1.5rem';
 });
 
